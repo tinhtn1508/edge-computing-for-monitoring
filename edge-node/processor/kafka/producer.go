2068713ee0871653479355ece84a4feb13808f98
Tuan Tran <TuanTranEngineer@github.com>
2020-10-29 23:47:08 +0700

    update kafka for edge processor

--- edge-node/processor/kafka/producer.go
+++ edge-node/processor/kafka/producer.go
@@ -11,12 +11,11 @@ import (
 
 // KafkaConfig is used to config for Kafka connection instances
 type KafkaConfig struct {
-	Host          string   `mapstructure:"host"`
-	Port          int      `mapstructure:"port"`
-	Brokers       []string `mapstructure:"brokers"`
-	DialerTimeout int      `mapstructure:"dialer-timeout"`
-	WriteTimeout  int      `mapstructure:"write-timeout"`
-	ReadTimeout   int      `mapstructure:"read-timeout"`
+	Host         string        `mapstructure:"host"`
+	Port         int           `mapstructure:"port"`
+	Brokers      []string      `mapstructure:"brokers"`
+	WriteTimeout time.Duration `mapstructure:"write-timeout"`
+	Topic        string        `mapstructure:"topic"`
 }
 
 type IProducer interface {
