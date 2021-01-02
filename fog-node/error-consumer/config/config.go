e9794931df58a6e890c8306340c5b061196ecc6f
Tinh Nguyen <tinh.nguyen@knorex.com>
2021-01-02 15:10:34 +0700

    temporary

--- fog-node/error-consumer/config/config.go
+++ fog-node/error-consumer/config/config.go
@@ -3,14 +3,24 @@ package config
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/spf13/viper"
 	"github.com/tinhtn1508/edge-computing-for-monitor/go-lib/kafka"
+	"github.com/tinhtn1508/edge-computing-for-monitor/go-lib/redis"
 )
+
+// _CatalogConfig ...
+type _CatalogConfig struct {
+	URL 	string			`mapstructure:"string"`
+	Timeout time.Duration	`mapstructure:"timeout"`
+}
 
 // Config holds all the configurations of the tool
 type Config struct {
 	KafkaConfig kafka.KafkaConfig `mapstructure:"kafka"`
+	RedisConfig redis.RedisConfig `mapstructure:"redis"`
+	CatalogConfig _CatalogConfig  `mapstructure:"catalog"`
 }
 
 // GetKafkaHost produces kafka hostname
