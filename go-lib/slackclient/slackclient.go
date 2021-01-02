090bf4a45a84768ea312dafd3f3a9ae5086d7d5a
Tinh Nguyen <tinh.nguyen@knorex.com>
2021-01-02 15:51:12 +0700

    update slack bot

--- go-lib/slackclient/slackclient.go
+++ go-lib/slackclient/slackclient.go
@@ -1,76 +1,61 @@
 package slackclient
 
 import (
-	"log"
-	"os"
+	"context"
+	"fmt"
+	"time"
 
-	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
+	"go.uber.org/zap"
 )
 
-var slackClient *SlackClient
-
-// SlackClient class
-type SlackClient struct {
-	client *slack.Client
+// ISlackClient ...
+type ISlackClient interface {
+	Send(string, string) error
 }
 
-// SlackClientInit method
-func (sc *SlackClient) SlackClientInit(token string) {
-	if token == "" {
-		log.Fatalln("Slacl Token not found")
+// SlackClientConf ...
+type SlackClientConf struct {
+	Token   string
+	Log     *zap.SugaredLogger
+	Ctx     context.Context
+	Timeout time.Duration
+}
+
+type slacklient struct {
+	client  *slack.Client
+	log     *zap.SugaredLogger
+	ctx     context.Context
+	timeout time.Duration
+}
+
+// NewSlackClient ...
+func NewSlackClient(conf SlackClientConf) ISlackClient {
+	return &slacklient{
+		log:     conf.Log,
+		client:  slack.New(conf.Token),
+		ctx:     conf.Ctx,
+		timeout: time.Duration,
 	}
-
-	log.Println("Slack Client Initializing ... ")
-	sc.client = slack.New(token)
 }
 
-//SendMessageToChanel method
-func (sc *SlackClient) SendMessageToChanel(channel string, message string) {
+func (sc *slackclient) Send(channel string, message string) error {
 	if channel == "" || message == "" {
-		log.Fatalf("Channel or message not found")
+		return fmt.Errorf("Channel or message not found")
 	}
 
-	channelID, timestamp, err := sc.client.PostMessage(
+	timeoutCtx, cancel := context.WithTimeout(sc.ctx, sc.timeout)
+	defer cancel()
+
+	channelID, timestamp, err := sc.client.PostMessageContext(
+		timeoutCtx,
 		channel,
 		slack.MsgOptionText(message, false),
 		slack.MsgOptionAsUser(true),
 	)
 	if err != nil {
-		log.Fatalf("ERROR: %s\n", err)
+		return fmt.Errorf("Error while sending message: %s", err)
 	}
-
-	log.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+	sc.log.Infof("Message successfully sent to channel %s at %s", channelID, timestamp)
+	return nil
 }
-
-// init module function
-func init() {
-	err := godotenv.Load("./slack.env")
-	if err != nil {
-		log.Fatal("Error loading slack.env file")
-	}
-
-	slackToken := os.Getenv("SLACK_TOKEN")
-
-	slackClient = &SlackClient{}
-	slackClient.SlackClientInit(slackToken)
-}
-
-// func main() {
-// 	err := godotenv.Load("./slack.env")
-// 	if err != nil {
-// 		log.Fatal("Error loading slack.env file")
-// 	}
-
-// 	slackToken := os.Getenv("SLACK_TOKEN")
-// 	log.Printf("slackToken: %s", slackToken)
-
-// 	sc := SlackClient{}
-// 	sc.SlackClientInit("xoxb-1607306712658-1607332830498-OCWytVekQjUHk80m7EUuzwe3")
-// 	// sc.SendMessageToChanel("#testpython", "Hello @gamil.com")
-
-// 	for i := 10; i <= 1; i-- {
-// 		sc.SendMessageToChanel("#testpython", "Hello @gamil.com")
-// 		time.Sleep(2)
-// 	}
-// }
