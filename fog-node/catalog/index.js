d298091d40ab150aa8e8051ffd3ea16c1ae5ed9a
Tinh Nguyen <tinh.nguyen@knorex.com>
2021-01-09 22:19:52 +0700

    remove unused code

--- fog-node/catalog/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const server = require("./server");
-console.log(process.env)
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => console.log(`API running on port ${port}`));
